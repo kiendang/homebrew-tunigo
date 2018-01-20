c976511540c5807841534fe681c3af0c5cab1d5b
kiendang <trungkiendang@hotmail.com>
2018-01-20 12:55:41 +0800

    Fix python package urls

--- python-mopidy-tunigo.rb
+++ python-mopidy-tunigo.rb
@@ -1,7 +1,7 @@
 class PythonMopidyTunigo < Formula
   desc "Mopidy extension for providing the browse feature of Spotify"
   homepage "https://github.com/trygveaa/mopidy-spotify-tunigo"
-  url "https://pypi.python.org/packages/7f/fd/d4259db55060293898b74e7ab51e83f1fd5baf26768909bf8d5c94f4b8f3/Mopidy-Spotify-Tunigo-1.0.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/7f/fd/d4259db55060293898b74e7ab51e83f1fd5baf26768909bf8d5c94f4b8f3/Mopidy-Spotify-Tunigo-1.0.0.tar.gz"
   sha256 "ed14484efc80536eb40d909b7d1cc789fd1f9565045c8408b387b92df4789737"
   head "https://github.com/trygveaa/mopidy-spotify-tunigo"
 
--- python-tunigo.rb
+++ python-tunigo.rb
@@ -1,7 +1,7 @@
 class PythonTunigo < Formula
   desc "Python API for the browse feature of Spotify"
   homepage "https://github.com/trygveaa/python-tunigo"
-  url "https://pypi.python.org/packages/41/3b/f9e4f04d142c064e62bbf9568332d13cb54e1109071641cad8123d99b3fb/tunigo-1.0.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/41/3b/f9e4f04d142c064e62bbf9568332d13cb54e1109071641cad8123d99b3fb/tunigo-1.0.0.tar.gz"
   sha256 "e2c90ee160fb59d48c553f8189a652d226229b64c091d235f675918dcc21b4a1"
   head "https://github.com/trygveaa/python-tunigo"
 
